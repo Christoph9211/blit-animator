2d4b41c712f2d45434d5a32ce5423c7839a622b4
Christoph9211 <chrisgibbons9211@gmail.com>
2025-04-21 16:40:19 -0500

    Enhance image loading and processing in TransparentVisionOverlay class

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import os
 import sys
 import cv2
 from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QVBoxLayout
-from PyQt5.QtGui import QImage, QPixmap
+from PyQt5.QtGui import QImage, QPixmap, QMovie
 from PyQt5.QtCore import Qt, QTimer
+import imageio.v2 as iio
 
 class TransparentVisionOverlay(QWidget):
     def __init__(self, animation_frames, fps=10):
@@ -14,7 +15,16 @@ class TransparentVisionOverlay(QWidget):
         self.timer = QTimer(self)
         self.timer.timeout.connect(self.update_frame)
         self.timer.start(1000 // fps)    # Adjust for your desired FPS
-
+# class TransparentVisionOverlay(QWidget):
+#     def __init__(self, apng_path, flags=Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool):
+#         super().__init__(flags=flags)
+#         self.setAttribute(Qt.WA_TranslucentBackground)
+#         self.label = QLabel(self)
+#         self.movie = QMovie(apng_path)
+#         self.movie.setCacheMode(QMovie.CacheAll)
+#         self.label.setMovie(self.movie)
+#         self.movie.start()
+        
     def initUI(self):
         # Set window flags for a frameless, always-on-top, transparent window
         self.setWindowFlags(Qt.FramelessWindowHint | Qt.WindowStaysOnTopHint | Qt.Tool)
@@ -45,7 +55,7 @@ class TransparentVisionOverlay(QWidget):
 
 if __name__ == "__main__":
     # Load images from folder
-    image_folder = 'transparent_frames'
+    image_folder = 'transparent_frames(40)'     # Path to your folder with images!
     images = []
     for img_name in os.listdir(image_folder):
         if img_name.endswith('.png'):
@@ -58,8 +68,20 @@ if __name__ == "__main__":
                 elif img.shape[2] == 4:
                     img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
                 images.append(img)
+                
+    # def save_apng(frames, fps, path="animation.apng"):
+    #     with iio.get_writer(path, mode="I", fps=fps) as writer:
+    #         for f in frames:
+    #             writer.append_data(f)   # frame must stay RGBA uint8
+    #     print(f"Saved {path}")
+        
     fps = 10
+    
+
     app = QApplication(sys.argv)
+    # window = TransparentVisionOverlay("animation.apng")
+
+    # save_apng(images, fps)
     window = TransparentVisionOverlay(images, fps)
     window.show()
     sys.exit(app.exec())
